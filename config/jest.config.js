ecf9195c35cb8a36f13705e72eff1ca6ae7c59e5
German <herman2000@yandex.ru>
2020-11-19 11:44:32 +0200

    1.2.1 * Added storybook * Moved tests to src

--- config/jest.config.js
+++ config/jest.config.js
@@ -2,8 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   moduleDirectories: ['node_modules', '../src'],
-  rootDir: '../__tests__',
-  setupFiles: ['<rootDir>/setupTests.ts'],
-  testMatch: ['**/__tests__/**/*.(spec|test).[jt]s?(x)'],
-
+  rootDir: '../src',
+  setupFiles: ['<rootDir>/setupTests.ts']
 };
