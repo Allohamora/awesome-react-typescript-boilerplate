78613b014be1b3cbadc369111b6fdd339b602855
German <herman2000@yandex.ru>
2020-11-21 01:17:28 +0200

    1.4.5 * Fixed conflicts between @typescript-eslint and prettier in .eslintrc * Fixed storybook scss import * Added storybook preview-head.html file * Updated storybook to 6.1.1

--- src/App/App.stories.tsx
+++ src/App/App.stories.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import App from 'App';
+import { Story, Meta } from '@storybook/react';
 
 export default {
   title: 'App',
   component: App,
-};
+} as Meta;
 
-export const Default = () => <App />;
-Default.storyName = 'default';
+export const Default: Story = () => <App />;
+Default.args = {};
