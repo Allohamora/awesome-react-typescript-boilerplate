5ef65bb17401def9258b514255251dcf40893830
German <herman2000@yandex.ru>
2020-11-20 14:33:42 +0200

    1.4.1 * Added pxToRem postcss plugin.

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -4,9 +4,14 @@ const { isProduction } = require('./utils');
 
 const postCssPresetEnv = require('postcss-preset-env');
 const cssNano = require('cssnano');
+const pxToRem = require('postcss-pxtorem');
 
 const plugins = [
-  postCssPresetEnv({ browsers: browserslist })
+  postCssPresetEnv({ browsers: browserslist }),
+  pxToRem({
+    rootValue: 10,
+    replace: true
+  })
 ];
 
 if( isProduction ) {
