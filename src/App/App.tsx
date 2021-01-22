1ac4af2201ec690d661807062ea7ae3c93d1b515
Allohamora <herman2000@yandex.ru>
2021-01-22 17:17:07 +0200

    Add ErrorBoundary

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import ErrorBoundary from 'components/ErrorBoundary';
 import AppProvider from './AppProvider';
 import logo from './img/logo.png';
 import { Block, Logo, Title } from './styles';
 
 const App: React.FC = () => (
-  <AppProvider>
-    <Block>
-      <Title>Awesome React Typescript Boilerplate</Title>
-      <Logo src={logo} alt="logo" />
-    </Block>
-  </AppProvider>
+  <ErrorBoundary>
+    <AppProvider>
+      <Block>
+        <Title>Awesome React Typescript Boilerplate</Title>
+        <Logo src={logo} alt="logo" />
+      </Block>
+    </AppProvider>
+  </ErrorBoundary>
 );
 
 export default App;
